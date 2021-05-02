ba2d77bc381d0dcc40c5aeba1b793655b9c99a87
Lose <lose0101@gmail.com>
2021-05-02 21:12:29 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
 <div>hello world，尚硅6
 <el-button>ok</el-button>
+{{msg}}
+<el-input v-model="msg"></el-input>
+{{number}}
+<el-slider v-model="number"></el-slider>
 </div>
 </template>
+<script>
+export default{
+    data(){
+        return{
+            msg:'hello world',
+            number:5,
+        }
+    }
+}
