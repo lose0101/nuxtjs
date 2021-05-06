ef18a58930f65204314b7c5489bed46fddc5b1f9
Lose <lose0101@gmail.com>
2021-05-06 22:01:15 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 <div>
-<el-button v-for="(x,index) in list" @click="c(index) :key="index">{{x}}</el-button>
+<el-button v-for="(x,index) in list" @click="c(index)">{{x}}</el-button>
 </div>
 <el-row v-if="index%2===0">
 <el-col :span="6" style="background:red">
