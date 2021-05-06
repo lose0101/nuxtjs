433f3dd497be65ad13152184e22297a0fafe1b77
Lose <lose0101@gmail.com>
2021-05-06 21:46:38 +0800

    Create index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
 <div>
 <div>
-<button v-for="x in list">{{x}}</button>
+<button v-for="(x,index) in list" @click="c(index)>{{x}}</button>
+<el-row v-if="index%2==0">
+<el-col :span="6" style="background:red">
+a
+</el-col>
+<el-col :span="18" style="background:green">
+b
+</el-col>
+</el-row>
+<el-row v-else>
+<el-col :span="18" style="background:green">
+b
+</el-col>
+<el-col :span="6" style="background:red">
+a
+</el-col>
+</el-row>
 </div>
 </div>
 </template>
@@ -9,8 +25,13 @@
 export default{
     data(){
         return {
+          index:0,
           list:[1,2,3,4]
         }
-    }
+    },
+    methods:{
+        c(index){
+             this.index=index
+        }
 }
 </script>
