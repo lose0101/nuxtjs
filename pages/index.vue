ed88a0cecac5b1f044959d577529d26d86f964c8
Lose <lose0101@gmail.com>
2021-05-06 21:32:36 +0800

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<div>
+<button v-if="x in list">{{x}}</button>
+</div>
+</div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+          list:[1,2,3,4]
+        }
+    }
+}
+</script>
