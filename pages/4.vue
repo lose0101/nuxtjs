24be7a40d874228021c236528eb68dbcb386cd7c
Lose <lose0101@gmail.com>
2021-05-08 15:59:47 +0800

    Create 4.vue

---
+++ pages/4.vue
@@ -0,0 +1,18 @@
+<template>
+{{msg}}
+<button @click="点击事件">ok</button>
+</template>
+<script>
+export default{
+  data(){
+    return {
+      msg:'你好世界'
+    }
+  },
+  methods:{
+    点击事件(){
+      this.msg='你好中国'
+    }
+  }
+}
+</script>
