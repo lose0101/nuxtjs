5ccdb5724cc5d4a5507e46564d5835fbaec1fe8d
Lose <lose0101@gmail.com>
2021-05-24 21:28:13 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,30 @@
 <h1>
 娜娜🐷老婆，老公超级爱你😘
 </h1>
-<div style="font-size:50px;text-align:center;">❤️</div>
+<div v-for="(x,index) in arr" :key="index"  style="text-align:center;" class="run">❤️</div>
 </div>
 </template>
+<script>
+export default{
+    data(){
+        return{
+            arr:[]
+        }
+    },
+    mounted(){
+        setInterval(()=>{
+            this.arr.push(1)
+        },300)
+    }
+}
+</scipt>
+<style>
+.run{
+animation:run all .5s infinite;
+}
+
+@keyframes run{
+    0%{font-size:50px;}
+    100%{font-size:150px;}
+}
+</style>
